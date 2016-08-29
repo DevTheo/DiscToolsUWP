7f9313a6934ea7b6b8c4feba415cbd39d7dccddb
jkimb <jkimb@DT-BARUCH3>
2016-08-28 20:24:47 -0400

    Add project files.

---
+++ DiscToolsLibUWP/Compatibility/AttributeUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscUtils.Partitions;
+using System.Reflection;
+
+namespace DiscUtils
+{
+    internal static class AttributeUtil
+    {
+        internal static IEnumerable<object> GetCustomAttributes(Type type, Type attr, bool v)
+        {
+            return type.GetTypeInfo().CustomAttributes.Where(ca => ca.GetType() == attr).Select( i => (object)i);
+        }
+        internal static object GetCustomAttribute(Type type, Type attr, bool v)
+        {
+            return GetCustomAttributes(type, attr, v).Where(i => i != null).FirstOrDefault();
+        }
+    }
+}
---
+++ DiscToolsLibUWP/Compatibility/CompatibilityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscUtils
+{
+    internal static class CompatibilityExtensions
+    {
+        internal static void Close(this Stream strm)
+        {
+            strm.Dispose();
+        }
+
+        internal static string ToUpper(this string str, CultureInfo ci)
+        {
+            return str.ToUpper();
+        }
+    }
+}
